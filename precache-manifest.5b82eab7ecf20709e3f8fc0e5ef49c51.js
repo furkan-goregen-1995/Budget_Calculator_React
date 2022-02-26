590cbaabfe0561cbfca9517ea7152f2e3c2678dc
Furkan Göregen <goregenfurkan@gmail.com>
2022-02-26 11:17:58 +0300

    Updates

---
+++ precache-manifest.5b82eab7ecf20709e3f8fc0e5ef49c51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72531d2c7e15b76dc43f",
+    "url": "/Budget_Calculator_React/static/css/main.bbb0df5e.chunk.css"
+  },
+  {
+    "revision": "72531d2c7e15b76dc43f",
+    "url": "/Budget_Calculator_React/static/js/main.72531d2c.chunk.js"
+  },
+  {
+    "revision": "e353791a0dd649119600",
+    "url": "/Budget_Calculator_React/static/js/1.e353791a.chunk.js"
+  },
+  {
+    "revision": "e26b0258d22d0bbcb8ca",
+    "url": "/Budget_Calculator_React/static/js/2.e26b0258.chunk.js"
+  },
+  {
+    "revision": "03a5670ec035a3bfd881",
+    "url": "/Budget_Calculator_React/static/js/runtime~main.03a5670e.js"
+  },
+  {
+    "revision": "5c91de3714ece71543641605d895a739",
+    "url": "/Budget_Calculator_React/index.html"
+  }
+];
